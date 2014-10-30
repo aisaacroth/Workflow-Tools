517f5bc94d68ec1785e03f45fa953190e43f0400
aisaacroth <aisaacroth@gmail.com>
2014-10-30 19:43:45 -0400

    Meta as fuck

--- committer.sh
+++ committer.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+while getopts ":p:" opt; do
+    case $opt in
+        p)
+            ./clean-wrap.sh $2
+            git add $2
+            git commit -m "$3" $2
+            git push
+            ;;
+    esac
+done
+
 ./clean-wrap.sh $1
 git add $1
 git commit -m "$2" $1
-
-while getopts ":a" opt; do
-    case $opt in
-        p)
-            git push
-            ;;
-   esac
-done

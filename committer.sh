17c4a854d45f4068458cc7a39716f3fc3d26024d
aisaacroth <aisaacroth@gmail.com>
2014-10-30 20:01:19 -0400

    Updated with push and usage functionality.

--- committer.sh
+++ committer.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-while getopts ":p:" opt; do
-    case $opt in
-        p)
-            ./clean-wrap.sh $2
-            git add $2
-            git commit -m "$3" $2
-            git push
-            ;;
+# committer = A script that cleans a file then commits it or pushes to git.
+
+function usage {
+    echo "usage: committer [[-p push ] [-h]]"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --push )    shift
+                         ./clean-wrap.sh $1
+                         git add $1
+                         git commit -m "$2" $1
+                         git push
+                         ;;
+        -h | --help )    usage
     esac
+    shift
 done
 
 if [ $# == 2 ] ; then

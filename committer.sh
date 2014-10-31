dd0aae673fac0db95fee7e1f32add15f1a8184fa
aisaacroth <aisaacroth@gmail.com>
2014-10-30 21:00:47 -0400

    Testing something

--- committer.sh
+++ committer.sh
@@ -11,11 +11,22 @@ while [ "$1" != "" ]; do
             shift
             ./clean-wrap.sh $1
             git add $1
-            git commit -m "$2" $1
+            shift
+            git commit -m "$1"
             git push
             ;;
         -h | --help )
             usage
+            ;;
+        -a | --all )
+            while [ $1 != "-c" ]; do
+                shift
+                echo "$1"
+                ./clean-wrap.sh $1
+                git add $1
+            done
+            echo "$2"
+            git commit -m "$2"
     esac
     shift
 done

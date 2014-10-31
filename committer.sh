6dd3ac6619269803aca55e7be6b9d5a5f5f334dd
aisaacroth <aisaacroth@gmail.com>
2014-10-30 21:13:23 -0400

    Updated and refactoring

--- committer.sh
+++ committer.sh
@@ -2,18 +2,26 @@
 # committer - A script that cleans a file then commits it or pushes to git.
 
 function usage {
-    echo "usage: committer [[-p push ] [-h]]"
+    echo "usage: committer [[-p push ] [-m message] [-h]]"
+}
+
+function clean_add {
+    ./clean-wrap.sh $1
+    git add $1
+}
+
+function commit_push {
+    git commit -m "$1"
+    git push
 }
 
 while [ "$1" != "" ]; do
     case $1 in
         -p | --push )
             shift
-            ./clean-wrap.sh $1
-            git add $1
+            clean_add $1
             shift
-            git commit -m "$1"
-            git push
+            commit_push "$1"
             ;;
         -h | --help )
             usage
@@ -21,20 +29,18 @@ while [ "$1" != "" ]; do
         -a | --all )
             while [ "$1" != "" ]; do
                 shift
-                if [ "$1" == "-c" ] ; then
+                if [ "$1" == "-m" || "$1" == "--message" ] ; then
                     break
                 fi
-                ./clean-wrap.sh $1
-                git add $1
+                clean_add $1
             done
-            git commit -m "$2"
+            commit_push "$2"
             ;;
     esac
     shift
 done
 
 if [ $# == 2 ] ; then
-    ./clean-wrap.sh $1
-    git add $1
+    clean_add $1
     git commit -m "$2" $1
 fi

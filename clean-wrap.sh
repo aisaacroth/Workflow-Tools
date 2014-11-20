9cafae3149adae33cdbaa042a736a4b56e66d5d7
aisaacroth <aisaacroth@gmail.com>
2014-11-20 15:45:58 -0500

    Added usage

--- clean-wrap.sh
+++ clean-wrap.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-python3 line_wrapper.py $1
-python3 clnzr.py $1
-echo "Cleaned $1"
+function usage {
+    echo "usage: clean-wrap <filename>"
+}
+
+if [ $# == 1 ] ; then
+    python3 line_wrapper.py $1
+    python3 clnzr.py $1
+    echo "Cleaned $1"
+else
+    usage
+fi

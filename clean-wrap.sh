be46aac0c4f62db68796adbb678b279900a6431e
aisaacroth <aisaacroth@gmail.com>
2016-01-17 20:24:36 -0500

    Updated gitignore.

--- clean-wrap.sh
+++ clean-wrap.sh
@@ -4,8 +4,8 @@ function usage {
 }
 
 if [ $# == 1 ] ; then
-    python3 line_wrapper.py $1
-    python3 clnzr.py $1
+    python3 /Users/aisaacroth/Programming/Projects/Workflow-Tools/line_wrapper.py $1
+    python3 /Users/aisaacroth/Programming/Projects/Workflow-Tools/clnzr.py $1
     echo "Cleaned $1"
 else
     usage
